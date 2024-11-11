d1679defe40842896363bc1de319f96a635cd994
Ricardo Ziglia <ricardorohnelt@gmail.com>
2024-11-11 11:15:48 -0300

    Setting up database schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,8 +1,50 @@
-import { integer, pgTable, varchar } from 'drizzle-orm/pg-core';
+import {
+  pgTable,
+  serial,
+  text,
+  integer,
+  varchar,
+  timestamp,
+} from 'drizzle-orm/pg-core';
+import { relations } from 'drizzle-orm';
 
-export const usersTable = pgTable('users', {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+export const users = pgTable('users', {
+  id: serial('id').primaryKey(),
   name: varchar({ length: 255 }).notNull(),
   age: integer().notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
+  createdAt: timestamp('createdAt').defaultNow().notNull(),
 });
+
+export const usersRelations = relations(users, ({ many }) => ({
+  todos: many(todos),
+}));
+
+export const todos = pgTable('todos', {
+  id: serial('id').primaryKey(),
+  title: varchar({ length: 255 }).notNull(),
+  userId: integer('user_id').notNull(),
+  createdAt: timestamp('createdAt').defaultNow().notNull(),
+});
+
+export const todosRelations = relations(todos, ({ one, many }) => ({
+  user: one(users, {
+    fields: [todos.userId],
+    references: [users.id],
+  }),
+  tasks: many(tasks),
+}));
+
+export const tasks = pgTable('tasks', {
+  id: serial('id').primaryKey(),
+  text: text('text'),
+  todoId: integer('todo_id').notNull(),
+  createdAt: timestamp('createdAt').defaultNow().notNull(),
+});
+
+export const commentsRelations = relations(tasks, ({ one }) => ({
+  post: one(todos, {
+    fields: [tasks.todoId],
+    references: [todos.id],
+  }),
+}));
